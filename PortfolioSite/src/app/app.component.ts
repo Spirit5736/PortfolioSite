ae179fadcf9ff67fbef78849d1b92a997fdc2895
Ivan Kozhevnikov <ivan.kozewnikow@gmail.com>
2024-07-15 23:02:07 +0200

    added about section

--- PortfolioSite/src/app/app.component.ts
+++ PortfolioSite/src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AboutComponent } from '../about/about.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AboutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'PortfolioSite';
+
+  scrollToSection(sectionId: string): void {
+    const element = document.getElementById(sectionId);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
 }
