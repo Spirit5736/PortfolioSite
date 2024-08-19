f66eaacfa01675e75e4d3a9170f50d48e6e3ea6f
Ivan Kozhevnikov <ivan.kozewnikow@gmail.com>
2024-08-19 21:53:28 +0200

    added animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AboutComponent } from '../about/about.component';
 
@@ -10,10 +10,33 @@ import { AboutComponent } from '../about/about.component';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  @HostListener('document:mousemove', ['$event'])
   title = 'PortfolioSite';
 
+
   constructor(
-  ) {}
+  ) { }
+
+  // Обработчик движения мыши
+  @HostListener('document:mousemove', ['$event'])
+  onMouseMove(event: MouseEvent) {
+    const image = document.getElementById('movingImage');
+    if (image) {
+      const screenWidth = window.innerWidth;
+      const screenHeight = window.innerHeight;
+
+      const centerX = screenWidth / 2;
+      const centerY = screenHeight / 2;
+
+      const offsetX = (event.clientX - centerX) / centerX;
+      const offsetY = (event.clientY - centerY) / centerY;
+
+      const moveX = offsetX * 10;
+      const moveY = offsetY * 10;
+
+      image.style.transform = `translate(-50%, -50%) translate(${moveX}px, ${moveY}px)`;
+    }
+  }
 
   scrollToSection(sectionId: string): void {
     const element = document.getElementById(sectionId);
@@ -22,3 +45,4 @@ export class AppComponent {
     }
   }
 }
+
