0fb043a7c331aa477f516ae92d900795f9540393
Ivan Kozhevnikov <ivan.kozewnikow@gmail.com>
2024-09-05 14:27:24 +0200

    commited

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,25 +20,25 @@ export class AppComponent {
   ) { }
 
   // Обработчик движения мыши
-  @HostListener('document:mousemove', ['$event'])
-  onMouseMove(event: MouseEvent) {
-    const image = document.getElementById('movingImage');
-    if (image) {
-      const screenWidth = window.innerWidth;
-      const screenHeight = window.innerHeight;
+  // @HostListener('document:mousemove', ['$event'])
+  // onMouseMove(event: MouseEvent) {
+  //   const image = document.getElementById('movingImage');
+  //   if (image) {
+  //     const screenWidth = window.innerWidth;
+  //     const screenHeight = window.innerHeight;
 
-      const centerX = screenWidth / 2;
-      const centerY = screenHeight / 2;
+  //     const centerX = screenWidth / 2;
+  //     const centerY = screenHeight / 2;
 
-      const offsetX = (event.clientX - centerX) / centerX;
-      const offsetY = (event.clientY - centerY) / centerY;
+  //     const offsetX = (event.clientX - centerX) / centerX;
+  //     const offsetY = (event.clientY - centerY) / centerY;
 
-      const moveX = offsetX * 10;
-      const moveY = offsetY * 10;
+  //     const moveX = offsetX * 10;
+  //     const moveY = offsetY * 10;
 
-      image.style.transform = `translate(-50%, -50%) translate(${moveX}px, ${moveY}px)`;
-    }
-  }
+  //     image.style.transform = `translate(-50%, -50%) translate(${moveX}px, ${moveY}px)`;
+  //   }
+  // }
 
   scrollToSection(sectionId: string): void {
     const element = document.getElementById(sectionId);
